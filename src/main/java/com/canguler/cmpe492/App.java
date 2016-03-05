c95be021691bdc60c8bcd905ec5bdc71fcdfe768
Can Güler <canguler93@gmail.com>
2016-03-05 12:08:04 +0200

    Implements table schema learning functionality and the deduction of the table name from a column name.

--- src/main/java/com/canguler/cmpe492/App.java
+++ src/main/java/com/canguler/cmpe492/App.java
@@ -4,23 +4,64 @@ import net.sf.jsqlparser.JSQLParserException;
 import net.sf.jsqlparser.expression.Expression;
 import net.sf.jsqlparser.expression.operators.relational.InExpression;
 import net.sf.jsqlparser.parser.CCJSqlParserUtil;
+import net.sf.jsqlparser.schema.Column;
+import net.sf.jsqlparser.schema.Table;
 import net.sf.jsqlparser.statement.Statement;
+import net.sf.jsqlparser.statement.create.table.ColumnDefinition;
+import net.sf.jsqlparser.statement.create.table.CreateTable;
+import net.sf.jsqlparser.statement.select.FromItem;
 import net.sf.jsqlparser.statement.select.PlainSelect;
 import net.sf.jsqlparser.statement.select.Select;
 import net.sf.jsqlparser.util.TablesNamesFinder;
 import net.sf.jsqlparser.util.deparser.ExpressionDeParser;
 import net.sf.jsqlparser.util.deparser.StatementDeParser;
 
-import java.util.List;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args ) throws JSQLParserException {
+public class App {
+    public static void main(String[] args) throws JSQLParserException, FileNotFoundException {
+        Scanner scanner = new Scanner(new File("create_table.sql"));
+        StringBuffer queryBuffer = new StringBuffer();
+        List<String> queries = new ArrayList<String>();
+        while (scanner.hasNextLine()) {
+            String nextLine = scanner.nextLine();
+            int length = nextLine.length();
+            if (length > 0) {
+                queryBuffer.append(nextLine);
+                queryBuffer.append('\n');
+                if (nextLine.charAt(nextLine.length() - 1) == ';') {
+                    queries.add(queryBuffer.toString());
+                    queryBuffer = new StringBuffer();
+                }
+            }
+        }
 
+        HashMap<String, Set<String>> tablesOfColumn = new HashMap<String, Set<String>>();
+        for (String query : queries) {
+            learnTableSchema(tablesOfColumn, query);
+        }
+        func1(tablesOfColumn);
+    }
+
+    public static void learnTableSchema(HashMap<String, Set<String>> tablesOfColumn, String createTableQueryString) throws FileNotFoundException, JSQLParserException {
+        CreateTable createTable = (CreateTable) CCJSqlParserUtil.parse(createTableQueryString);
+        Table table = createTable.getTable();
+        List<ColumnDefinition> columnDefinitions = createTable.getColumnDefinitions();
+        for (ColumnDefinition columnDefinition : columnDefinitions) {
+            String columnName = columnDefinition.getColumnName().toLowerCase();
+            if (!tablesOfColumn.containsKey(columnName)) {
+                tablesOfColumn.put(columnName, new HashSet<String>());
+            }
+            tablesOfColumn.get(columnName).add(table.getName().toLowerCase());
+        }
+    }
+
+    public static void func1(HashMap<String, Set<String>> tablesOfColumnMap) throws JSQLParserException {
         Statement statement = CCJSqlParserUtil.parse("select\n" +
                 "\tsum(l_extendedprice * (1 - l_discount) ) as revenue\n" +
                 "from\n" +
@@ -60,12 +101,35 @@ public class App
         PlainSelect plainSelect = (PlainSelect) selectStatement.getSelectBody();
         PlainSelectVisitor plainSelectVisitor = new PlainSelectVisitor();
         plainSelect.accept(plainSelectVisitor);
-        System.out.println("Tables: " + plainSelectVisitor.fromItems);
-        System.out.println("Columns: " + plainSelectVisitor.getColumnSet());
+
+
+        Set<String> tableNames = new HashSet<String>();
+        for (FromItem fromItem : plainSelectVisitor.fromItems) {
+            String tableName = fromItem.toString().toLowerCase();
+            tableNames.add(tableName);
+        }
+
+        Set<Column> columnSet = plainSelectVisitor.getColumnSet();
+
+        for (Column column : columnSet) {
+            String columnName = column.getColumnName();
+            if (tablesOfColumnMap.containsKey(columnName)) {
+                Set<String> tablesOfColumn = tablesOfColumnMap.get(columnName);
+
+                Set<String> intersection = new HashSet<String>(tablesOfColumn);
+                intersection.retainAll(tableNames);
+
+                if (intersection.size() == 1) {
+                    System.out.println(columnName + " : " + intersection);
+                } else {
+                    System.out.println(columnName + " : " + intersection);
+                }
+            }
+        }
+//        System.out.println("Columns: " + plainSelectVisitor.getColumnSet());
 
 
 //        Expression a = CCJSqlParserUtil.parseExpression("p_container in ('SM CASE', 'SM BOX', 'SM PACK', 'SM PKG')");
 //        System.out.println(a);
-
     }
 }
